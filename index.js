fe368d829d8a95ee0b233aeac97f030cd6f77b08
Busybee <jiwoooookim@gmail.com>
2020-05-01 15:33:54 +0100

    Add basic files

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+// http://localhost:3000/recipes/index.html
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/hello', (req, res) => res.send('Hello'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)) 
