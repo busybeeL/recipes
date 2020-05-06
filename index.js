f38c2da21a03b0deb701be22dd98df218bdc9f38
Busybee <jiwoooookim@gmail.com>
2020-05-06 09:22:21 +0100

    Installed bootstrap package and updated gitignore file

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/hello', (req, res) => res.send('Hello'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)) 
+
+
